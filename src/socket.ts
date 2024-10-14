8445475e006a08cacbd181ddf7785a5965fcc75b
Pankaj <pankajkumarhzb787@gmail.com>
2024-10-14 21:16:31 +0530

    improve cors

--- src/socket.ts
+++ src/socket.ts
@@ -1,16 +1,24 @@
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import logger from "./config/logger";
+import config from "config";
 
 const wsServer = createServer();
 
-const io = new Server(wsServer, { cors: { origin: "http://localhost:5173" } });
+const ALLOWED_DOMAINS = [
+    config.get("frontend.clientUI"),
+    config.get("frontend.adminUI"),
+] as string[];
+
+const io = new Server(wsServer, { cors: { origin: ALLOWED_DOMAINS } });
 
 io.on("connection", (socket) => {
     logger.info("Client connected", socket.id);
 
     socket.on("join", (data) => {
         socket.join(String(data.tenantId));
+
+        console.log("User joined on room:", data.tenantId);
 
         socket.emit("join", { roomId: String(data.tenantId) });
     })
